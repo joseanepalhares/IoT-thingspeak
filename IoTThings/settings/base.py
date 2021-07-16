c43df14abb66cbe8dd7f20bb2f9ef723d507d641
joseanepalhares <joseanepalhares@gmail.com>
2021-07-15 21:00:49 -0300

    Commit Thingspeak

--- IoTThings/settings/base.py
+++ IoTThings/settings/base.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
+django_heroku.settings(locals())
